9c60fb51c965cd3bad307efa33e494b9d4a261e1
HopeCortes <158113822+HopeCortes@users.noreply.github.com>
2024-02-05 21:04:36 -0500

    Update sumlist.py

--- sumlist.py
+++ sumlist.py
@@ -1,11 +1,11 @@
-y = [1,9,5,0,20,-4,12,16,7]
-len(y)
-suma = 12
-for i in range(0,len(y)):
+y = [1,9,5,0,20,-4,12,16,7] # Se asignan valores a la lista creada
+len(y) # Se recorre la lista de datos asignados a la lista
+suma = 12 # El objetivo es buscar en la lista los pares de números que sumados den 12
+for i in range(0,len(y)): # Se itera para el primer número de la lista
 
-  for j in range(i+1,len(y)):
-      res=y[i]+y[j]
+  for j in range(i+1,len(y)): # Se itera para el segundo número de la lista
+      res=y[i]+y[j] # Se almacenan los resultados de la iteración que cumplan on el objetivo
 
       if res == suma:
-        print(y[i], ", ", y[j])
+        print(y[i], ", ", y[j]) # Se muestran las parejas de números que cumplen con el objetivo
 
